fcbed38b2d0f2102a5282c8efed985fca17ecf8f
Shivam Pandey <101188396+shivam72003@users.noreply.github.com>
2025-04-13 14:15:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ const store = mongoStoreSession({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
-  console.log(`error in session store`);
-});
+// store.on("error", () => {
+//   console.log(`error in session store`);
+// });
 
 sessionOptions = {
   store,
